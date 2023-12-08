0345ce5f1b34b00af516a863d241f989a42cc733
sidhanathdon <sid55boss@gmail.com>
2023-12-08 11:22:00 +0530

    changes updated

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,21 @@ import ItemList from '../components/ItemList';
 export default function Home() {
     const [items, setItems] = useState([]);
     const [message, setMessage] = useState('');
+    const addItem = useCallback(() => {
+        const newItem = `Item ${items.length + 1}`;
+        setItems(prevItems => [...prevItems, newItem]);
+        setMessage('Item added successfully!');
 
-    
+        // Clear message after 3 seconds
+        setTimeout(() => {
+            setMessage('');
+        }, 3000);
+    }, [items]);
+
 
     return (
         <div>
-            <button >Add Item</button>
+            <button onClick={addItem} >Add Item</button>
             <ItemList items={items} />
             {message && <p id='message'>{message}</p>}
         </div>
